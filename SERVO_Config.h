69f0ea9ec48321dc8baf88d667500f66ec5b3e69
mennatullahtammer-tech <mennatullahtammer@gmail.com>
2026-08-29 21:06:22 +0300

    Delete SERVO_Config.h

--- SERVO_Config.h
+++
@@ -1,9 +0,0 @@
-#ifndef MCAL_TIMER_TIMER_CONFIG_H_
-#define MCAL_TIMER_TIMER_CONFIG_H_
-
-#define TIMER1_SERVO_TOP       19999
-
-#define TIMER1_SERVO_MIN       1000
-#define TIMER1_SERVO_MAX       2000
-
-#endif
